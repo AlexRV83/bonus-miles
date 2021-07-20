b25e545d7170c5570bddfe7b318e51aaba3cff97
AlexRV83 <85925901+AlexRV83@users.noreply.github.com>
2021-07-20 23:35:58 +0300

    program works

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 10513;
+        int mile = 20;
+        int bonus = ticket / mile;
+        System.out.println(bonus);
+}
+}
